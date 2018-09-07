532a25581b7ef81064ae676b5746f18d194ed389
Istvan Sajtos <istvan.sajtos@liferay.com>
2018-09-07 11:42:31 +0200

    GROW-465 Autogenerated

---
+++ modules/wiki-customizer/wiki-customizer-api/src/main/java/com/liferay/wiki/customizer/service/service/WikiPageCustomLocalService.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.wiki.customizer.service.service;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.service.BaseLocalService;
+import com.liferay.portal.kernel.transaction.Isolation;
+import com.liferay.portal.kernel.transaction.Transactional;
+
+/**
+ * Provides the local service interface for WikiPageCustom. Methods of this
+ * service will not have security checks based on the propagated JAAS
+ * credentials because this service can only be accessed from within the same
+ * VM.
+ *
+ * @author Brian Wing Shun Chan
+ * @see WikiPageCustomLocalServiceUtil
+ * @see com.liferay.wiki.customizer.service.service.base.WikiPageCustomLocalServiceBaseImpl
+ * @see com.liferay.wiki.customizer.service.service.impl.WikiPageCustomLocalServiceImpl
+ * @generated
+ */
+@ProviderType
+@Transactional(isolation = Isolation.PORTAL, rollbackFor =  {
+	PortalException.class, SystemException.class})
+public interface WikiPageCustomLocalService extends BaseLocalService {
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never modify or reference this interface directly. Always use {@link WikiPageCustomLocalServiceUtil} to access the wiki page custom local service. Add custom service methods to {@link com.liferay.wiki.customizer.service.service.impl.WikiPageCustomLocalServiceImpl} and rerun ServiceBuilder to automatically copy the method declarations to this interface.
+	 */
+
+	/**
+	* Returns the OSGi service identifier.
+	*
+	* @return the OSGi service identifier
+	*/
+	public java.lang.String getOSGiServiceIdentifier();
+}
